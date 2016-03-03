e1a9d807ce25131351345bcf03b47959fa702796
anwarshk17 <anwarshaikh17@hotmail.com>
2016-03-03 17:13:25 +0530

    Add Glide and background

--- app/src/main/java/ceberus/anwar/bucketdrops/ActivityMain.java
+++ app/src/main/java/ceberus/anwar/bucketdrops/ActivityMain.java
@@ -2,12 +2,37 @@ package ceberus.anwar.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mbtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mbtnAdd = (Button) findViewById(R.id.btn_add);
+        mbtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this,"Button was clicked",Toast.LENGTH_SHORT);
+            }
+        });
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(background);
     }
 }
